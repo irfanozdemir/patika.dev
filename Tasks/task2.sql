9f34d7b9afdbebfce11897158a687f17e81f3b27
irfan <irfanozdemirr@gmail.com>
2022-10-13 01:07:09 +0300

    task2 dosyasi eklendi.

--- Tasks/task2.sql
+++ Tasks/task2.sql
@@ -1,4 +1,4 @@
--- 1. Film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız (BETWEEN-AND yapısını kullanınız.)
+-- 1. Film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız. (BETWEEN-AND yapısını kullanınız.)
 SELECT * FROM film 
 WHERE (replacement_cost BETWEEN 12.99 AND 16.99) AND replacement_cost != 16.99 ORDER BY replacement_cost ASC;
 
